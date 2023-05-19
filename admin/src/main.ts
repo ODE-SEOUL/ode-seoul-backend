a39e9529e62dc5276e08458f03d0ceca2aa60751
Na Jongwoo <njw1204@naver.com>
2023-05-19 15:11:40 +0900

    fix: 어드민 이미지 업로드 버그 수정

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   process.env.TZ = 'Asia/Seoul';
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
   await app.listen(5246);
 }
 

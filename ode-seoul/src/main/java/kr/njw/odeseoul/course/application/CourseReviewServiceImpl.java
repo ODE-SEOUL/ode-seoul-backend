278766d897de42c62b6c1f578819a4e66c4f497f
Na Jongwoo <njw1204@naver.com>
2023-05-21 17:36:14 +0900

    style: DB 유니크 키로 중복 저장 방지하여 TODO 제거

--- ode-seoul/src/main/java/kr/njw/odeseoul/course/application/CourseReviewServiceImpl.java
+++ ode-seoul/src/main/java/kr/njw/odeseoul/course/application/CourseReviewServiceImpl.java
@@ -36,7 +36,6 @@ public class CourseReviewServiceImpl implements CourseReviewService {
         }
 
         if (this.courseReviewRepository.findFirstByCourseIdAndUserIdAndDeletedAtIsNull(request.getCourseId(), request.getUserId()).isPresent()) {
-            // TODO: 짧은 시간 내 요청시 중복 작성 방지
             throw new BaseException(BaseResponseStatus.WRITE_COURSE_REVIEW_ERROR_ALREADY_WRITTEN);
         }
 
